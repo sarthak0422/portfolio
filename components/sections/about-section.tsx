d811fbf534a12600088ef170435277c69f9dc6c9
Halloweenking <sarthakhulk22@gmail.com>
2025-10-28 18:44:32 +0530

    Resume Updated

--- components/sections/about-section.tsx
+++ components/sections/about-section.tsx
@@ -157,7 +157,7 @@ export function AboutSection() {
                 variants={itemVariants}
                 className="mt-8 flex flex-col sm:flex-row gap-4"
               >
-                <a href="/files/SarthakSureshTambdeResume_U.pdf" download="Sarthak_Tambde_Resume.pdf">
+                <a href="/files/SarthakSureshTambdeResume_A.pdf" download="Sarthak_Tambde_Resume.pdf">
                 <Button className="group">
                   <Download className="mr-2 h-4 w-4 transition-transform group-hover:-translate-y-1" />
                   Download Resume
